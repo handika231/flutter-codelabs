386c3f080c0c33b1a7293bd35a9747f436a9e36e
Handika Rafli A <shandika231@gmail.com>
2022-08-05 14:51:14 +0700

    create ui

--- generation_app/lib/ui/home_page.dart
+++ generation_app/lib/ui/home_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+  HomePage({Key? key}) : super(key: key);
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Generation App'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                maxLength: 4,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Enter your year of birth',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                child: const Text('Generate'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
