e3ac58750fc63ccd165a8eed1221c5c26f708485
Handika Rafli A <shandika231@gmail.com>
2022-08-15 10:09:56 +0700

    animate a page route transition

--- animation_flutter_app/lib/pages/home_page.dart
+++ animation_flutter_app/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'other_page.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -7,11 +9,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
         title: const Text('Home Page'),
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Other Page'),
+              onPressed: () {
+                Navigator.push(context, _createRoute());
+              },
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return const OtherPage();
+      },
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        print(child);
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+        final tween = Tween(begin: begin, end: end);
+        final curveAnimation = CurvedAnimation(parent: animation, curve: curve);
+// TODO: Other way to do this
+        // final curveTween = CurveTween(curve: curve);
+        // final tween = Tween(begin: begin, end: end).chain(curveTween);
+        // final offsetAnimation = animation.drive(tween);
+        return SlideTransition(
+          position: tween.animate(curveAnimation),
+          child: child,
+          // position: offsetAnimation,
+        );
+      },
     );
   }
 }
