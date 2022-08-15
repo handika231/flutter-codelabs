afc441791fc892e184a39a320bab95a2589f931a
Handika Rafli A <shandika231@gmail.com>
2022-08-15 09:44:46 +0700

    add project structure

---
+++ animation_flutter_app/lib/application/app/application.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../pages/pages.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
