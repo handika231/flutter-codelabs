39fce3f1660c1fa72aadf50497f7211601cc63b7
Handika Rafli A <shandika231@gmail.com>
2022-08-17 10:35:35 +0700

    clear news app v1

--- news_app_local/lib/pages/setting_page.dart
+++ news_app_local/lib/pages/setting_page.dart
@@ -1,10 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_local/config/platform_widget.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key}) : super(key: key);
   static const routeName = 'setting';
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(routeName),
+        centerTitle: true,
+      ),
+      body: _buildList(context),
+    );
+  }
+
+  Widget _buildIOS(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: _buildList(context),
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text(routeName),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return PlatformWidget(androidBuilder: _buildAndroid, iosBuilder: _buildIOS);
+  }
+
+  _buildList(BuildContext context) {
+    return ListView(
+      children: [
+        Material(
+          child: ListTile(
+            title: const Text('Dark Theme'),
+            trailing: Switch.adaptive(
+              value: false,
+              onChanged: (val) {
+                defaultTargetPlatform == TargetPlatform.iOS
+                    ? showCupertinoDialog(
+                        context: context,
+                        builder: (context) {
+                          return CupertinoAlertDialog(
+                            title: const Text('Cooming Soon'),
+                            content: const Text(
+                                'This feature is not available yet.'),
+                            actions: [
+                              CupertinoDialogAction(
+                                child: const Text('OK'),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                            ],
+                          );
+                        },
+                      )
+                    : showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Cooming Soon!'),
+                            content: const Text(
+                                'This feature is not available yet.'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text(
+                                  'Oke',
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
