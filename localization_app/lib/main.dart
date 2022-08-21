239aa2c36bbe4b13a093e2c9440a7c41aa99a170
Handika Rafli A <shandika231@gmail.com>
2022-08-21 13:43:38 +0700

    add localization

---
+++ localization_app/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Application Localization',
+      home: HomeView(),
+      localizationsDelegates: [
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('id', ''), // Indo, no country code
+        Locale('en', ''), // English, no country code
+      ],
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          AppLocalizations.of(context)?.helloWorld("Jokowi", "Semarang") ?? "Dika",
+          style: const TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
